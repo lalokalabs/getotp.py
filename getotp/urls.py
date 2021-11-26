5f94e781a4a379c4ac2dddb8ff093c114950a992
Kamal Mustafa <kamal@xoxzo.com>
2021-11-26 06:30:07 +0000

    remove unused imports

--- getotp/urls.py
+++ getotp/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from getotp.views import (
-    signup_callback,
-    login_callback,
-    verification_success,
     login_start,
+    login_callback,
     login_complete,
 )
 
 urlpatterns = [
     path("login/start/", login_start, name="login-start"),
-    path("login/complete/", login_complete, name="login-complete"),
-    path('signup_callback/', signup_callback, name="signup-callback"),
     path('login/callback/', login_callback, name="login-callback"),
-    path("verification_success/", verification_success, name="verification-success"),
+    path("login/complete/", login_complete, name="login-complete"),
 ]
